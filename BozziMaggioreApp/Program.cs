3bdf4bf978afb5bbe58cffc06bb2d632b7b9d8f4
Marcello Bozzi <bozzimarcello@gmail.com>
2022-11-07 11:09:27 +0100

    Aggiungere i file di progetto.

---
+++ BozziMaggioreApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BozziMaggioreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // leggo i valori dalla tastiera
+            string xStringa = Console.ReadLine();
+            string yStringa = Console.ReadLine();
+
+            // converto le stringhe in interi
+            int x = int.Parse(xStringa);
+            int y = int.Parse(yStringa);
+
+        }
+    }
+}
