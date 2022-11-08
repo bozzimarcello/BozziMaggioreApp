1f2b8a33396d384edf8c5e1d80a9bf8e283e9d65
Marcello Bozzi <bozzimarcello@gmail.com>
2022-11-08 11:50:46 +0100

    completato
    completato l'algoritmo e aggiunto i messaggi

--- BozziMaggioreApp/Program.cs
+++ BozziMaggioreApp/Program.cs
@@ -6,13 +6,26 @@ namespace BozziMaggioreApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Questo programma trova il maggiore fra due numeri");
+
             // leggo i valori dalla tastiera
+            Console.WriteLine("Inserisci il primo numero poi premi invio");
             string xStringa = Console.ReadLine();
+            Console.WriteLine("Inserisci il secondo numero poi premi invio");
             string yStringa = Console.ReadLine();
 
             // converto le stringhe in interi
-            int x = int.Parse(xStringa);
-            int y = int.Parse(yStringa);
+            int primoNumero = int.Parse(xStringa);
+            int secondoNumero = int.Parse(yStringa);
+
+            if (primoNumero>secondoNumero) 
+            {
+                Console.WriteLine("Il maggiore è il numero: " + primoNumero);
+            }
+            else
+            {
+                Console.WriteLine("Il maggiore è il numero: " + secondoNumero);
+            }
 
         }
     }
